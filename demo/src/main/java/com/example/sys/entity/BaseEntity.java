ae8dd8617a6a7ff705795861c3388746e91ebc96
xiashisong <2582547412@qq.com>
2021-01-26 20:52:42 +0800

    删除

--- demo/src/main/java/com/example/sys/entity/BaseEntity.java
+++
@@ -1,9 +0,0 @@
-package com.example.sys.entity;
-
-import java.io.Serializable;
-import java.util.Date;
-
-public class BaseEntity implements Serializable {
-    public Integer createUserId;
-    public Date createTime;
-}
--- demo/src/main/java/com/example/sys/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.sys.service;
-
-import com.baomidou.mybatisplus.service.IService;
-import com.example.sys.entity.User;
-
-
-public interface UserService extends IService<User> {
-    public User findByUsername(String username);
-}
