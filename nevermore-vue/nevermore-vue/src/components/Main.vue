92222ce03dc4b4b6213e01375ef329c8aa4ffbe2
xiashisong <2582547412@qq.com>
2021-01-27 11:07:18 +0800

    vue代码

---
+++ nevermore-vue/nevermore-vue/src/components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="main">
+<h1>{{ title }}</h1>
+</div>
+</template>
+<script>
+export default {
+name: "Main",
+data:function () {
+return{
+title:'主页'
+}
+}
+}
+</script>
+<!-- 局部的要写scoped-->
+<style scoped>
+.main {
+/*vh:相对于视窗的高度，那么vw:则是相对于视窗的宽度*/
+height: 100vh; /*100vh代表网页撑满一个屏*/
+background-color: orange;
+}
+h1 {
+margin: 0; /*去除h1标签自带的margin边距*/
+color: red;
+}
+</style>
